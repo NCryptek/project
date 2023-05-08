aad46997a670bf35037fae0119ddd94c833ce23e
Kapepx <johndoe@example.com>
2023-05-08 14:03:21 +0200

    Zmiany w klasie jednostki

--- src/globals.py
+++ src/globals.py
@@ -9,3 +9,16 @@ unitList = []
 
 currentPlayer = 0
 currentUnit = None
+
+def Dist(x1, y1, x2, y2):
+    return abs(x2 - x1) + abs(y2 - y1)
+
+def GetPathFromTo(x1, y1, x2, y2):
+    return
+
+def IsFreeFor(unit, x, y):
+    dest = tileList[y * mapSizeX + x]
+    if (dest != unit and dest != None):
+        return False
+    else:
+        return True
