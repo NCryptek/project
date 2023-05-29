a1248c779bc1340181f904ed53828bfd71f11fde
Kapepx <johndoe@example.com>
2023-05-29 18:11:23 +0200

    keyhandler_move + rendermap

--- game.py
+++ game.py
@@ -6,6 +6,9 @@ import keyboard
 import random
 import colorama
 
+cursorX = 0
+cursorY = 0
+
 def StartGame():
     textRenderer.clearScreen()
     globals.defaultRenderer.Resize(globals.camSizeX * 4 + 1, globals.camSizeY * 2 + 1)
@@ -53,6 +56,7 @@ def KeyHandler_Basic(key):
         handled = True
         pass
     elif (key == "c"):
+        globals.gameState = 4
         handled = True
         pass
     elif (key == "1"):
@@ -80,7 +84,25 @@ def KeyHandler_Attack(key):
     pass
 
 def KeyHandler_Camera(key):
-    pass
+    handled = False
+    if (key == "left"):
+        if (globals.camOffsetX > 0):
+            globals.camOffsetX -= 1
+        handled = True
+    elif (key == "up"):
+        if (globals.camOffsetY > 0):
+            globals.camOffsetY -= 1
+        handled = True
+    elif (key == "right"):
+        if (globals.camOffsetX + globals.camSizeX < globals.mapSizeX - 1):
+            globals.camOffsetX += 1
+        handled = True
+    elif (key == "down"):
+        if (globals.camOffsetY + globals.camSizeY < globals.mapSizeY - 1):
+            globals.camOffsetY += 1
+        handled = True
+    
+    return handled
 
 def QuitPrompt():
     textRenderer.clearScreen()
@@ -92,8 +114,22 @@ def QuitPrompt():
     textRenderer.clearScreen()
 
 def RenderMap():
-    globals.defaultRenderer.GenGrid(0, 0, globals.camSizeX * 4 + 1, globals.camSizeY * 2 + 1, 3, 1)
-    globals.defaultRenderer.FillChar()
+    rightmost = globals.camSizeX * 4 + 1
+    downmost = globals.camSizeY * 2 + 1
+
+    globals.defaultRenderer.GenGrid(0, 0, rightmost, downmost, 3, 1)
+    if (globals.camOffsetY > 0):
+        globals.defaultRenderer.FillChar("↑", 0, 0, rightmost, 0)
+
+    if (globals.camOffsetX + globals.camSizeX < globals.mapSizeX - 1):
+        globals.defaultRenderer.FillChar("→", rightmost - 1, 0, rightmost - 1, downmost - 2)
+
+    if (globals.camOffsetY + globals.camSizeY < globals.mapSizeY - 1):
+        globals.defaultRenderer.FillChar("↓", 0, downmost - 1, rightmost - 2, downmost - 1)
+
+    if (globals.camOffsetX > 0):
+        globals.defaultRenderer.FillChar("←", 0, 0, 0, downmost - 2)
+
     for i in globals.unitList:
         if (i.health == 0):
             continue
@@ -102,9 +138,11 @@ def RenderMap():
         elif (i.posY < globals.camOffsetY or i.posY >= globals.camOffsetY + globals.camSizeY):
             continue
 
-        globals.defaultRenderer.SetChar("O", (i.posX - globals.camOffsetY) * 4 + 2, (i.posY - globals.camOffsetY) * 2 + 1)
-        globals.defaultRenderer.SetColor(textRenderer.GREEN << 4, (i.posX - globals.camOffsetY) * 4 + 2, (i.posY - globals.camOffsetY) * 2 + 1)
-        
+        globals.defaultRenderer.SetChar("O", (i.posX - globals.camOffsetX) * 4 + 2, (i.posY - globals.camOffsetY) * 2 + 1)
+        if (i.ownerId == 0):
+            globals.defaultRenderer.SetColor(textRenderer.GREEN << 4, (i.posX - globals.camOffsetX) * 4 + 2, (i.posY - globals.camOffsetY) * 2 + 1)
+        else:
+            globals.defaultRenderer.SetColor(textRenderer.RED << 4, (i.posX - globals.camOffsetX) * 4 + 2, (i.posY - globals.camOffsetY) * 2 + 1)
 
     globals.defaultRenderer.Overwrite()
 
